2ab76ae1ba529d0f670a94063a9f2eb7b02f5d5d
Lisanna Paladin <lisanna.paladin@embl.de>
2022-12-14 17:01:19 +0100

    based on tab data, builds json, skips levels

--- backup_old_version/classification.js
+++ backup_old_version/classification.js
@@ -1,10 +1,42 @@
 let classification = [
     {
+        "id": "1",
+        "full_id":"1",
+        "parent":"1",
+        "type":"skill",
+        "short_description":"Skill A",
+        "link":""
+    },
+    {
+        "id": "1",
+        "full_id":"1.1",
+        "parent":"1",
+        "type":"course",
+        "short_description":"Course 1",
+        "link":""
+    },
+    {
+        "id": "2",
+        "full_id":"1.2",
+        "parent":"1",
+        "type":"course",
+        "short_description":"Course 2",
+        "link":""
+    },
+    {
+        "id": "3",
+        "full_id":"1.3",
+        "parent":"1",
+        "type":"course",
+        "short_description":"Course 3",
+        "link":""
+    },
+    {
         "id": "2",
         "full_id":"2",
         "parent":"2",
         "type":"skill",
-        "short_description":"Skill A",
+        "short_description":"Skill B",
         "link":""
     },
     {
@@ -12,45 +44,13 @@ let classification = [
         "full_id":"2.1",
         "parent":"2",
         "type":"course",
-        "short_description":"Course 1",
+        "short_description":"Course 4",
         "link":""
     },
     {
         "id": "2",
         "full_id":"2.2",
         "parent":"2",
-        "type":"course",
-        "short_description":"Course 2",
-        "link":""
-    },
-    {
-        "id": "3",
-        "full_id":"2.3",
-        "parent":"2",
-        "type":"course",
-        "short_description":"Course 3",
-        "link":""
-    },
-    {
-        "id": "3",
-        "full_id":"3",
-        "parent":"1",
-        "type":"skill",
-        "short_description":"Skill B",
-        "link":""
-    },
-    {
-        "id": "1",
-        "full_id":"3.1",
-        "parent":"3",
-        "type":"course",
-        "short_description":"Course 4",
-        "link":""
-    },
-    {
-        "id": "2",
-        "full_id":"3.2",
-        "parent":"3",
         "type":"course",
         "short_description":"Course 5",
         "link":""
